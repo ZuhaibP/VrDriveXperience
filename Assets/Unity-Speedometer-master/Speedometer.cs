144bada3bca138c8926a4a8b8d94b4ff7d6793b5
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-05-22 00:04:24 +0200

    SpeedOmeter is working and changed tires

---
+++ Assets/Unity-Speedometer-master/Speedometer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Speedometer : MonoBehaviour
+{
+    public Rigidbody target;
+
+    public float maxSpeed = 0.0f; // The maximum speed of the target ** IN KM/H **
+
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
+
+    [Header("UI")]
+    public TextMeshProUGUI speedLabel;// The label that displays the speed;
+    public MeshRenderer arrowRenderer; // The arrow in the speedometer
+
+    private float speed = 0.0f;
+    private void Update()
+    {
+        // 3.6f to convert in kilometers
+        // ** The speed must be clamped by the car controller **
+        speed = target.velocity.magnitude * 3.6f;
+
+        if (speedLabel != null)
+            speedLabel.text = ((int)speed) + " km/h";
+        if (arrowRenderer != null)
+        {
+            arrowRenderer.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed / maxSpeed));
+        }
+    }
+}
