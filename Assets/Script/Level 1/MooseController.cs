b36f8d5944d1c76369586c445499de3c80472175
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-06-13 03:01:15 +0200

    Moose is working from start position to target position

--- Assets/Script/Level 1/MooseController.cs
+++ Assets/Script/Level 1/MooseController.cs
@@ -4,18 +4,22 @@ public class MooseController : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private Transform targetPosition;
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform mooseActivator; // New position for activating the moose
     private Animator animator;
     private bool isWalking = false;
+    private bool isActive = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         isWalking = true;
+        transform.position = startPosition.position;
     }
 
     private void Update()
     {
-        if (isWalking)
+        if (isActive && isWalking)
         {
             Vector3 direction = (targetPosition.position - transform.position).normalized;
             transform.position += direction * walkSpeed * Time.deltaTime;
@@ -23,15 +27,32 @@ public class MooseController : MonoBehaviour
             if (transform.position.x >= targetPosition.position.x)
             {
                 isWalking = false;
-                animator.SetBool("IsWalking", false); // Update the animation parameter
+                animator.SetBool("IsWalking", false);
+                Destroy(gameObject);
             }
         }
     }
 
     public void StartWalking()
     {
-        animator.SetBool("IsWalking", true); // Update the animation parameter
-        targetPosition = GameObject.Find("TargetPosition").transform; // Replace "TargetPosition" with the actual name of the GameObject representing the target position
+        animator.SetBool("IsWalking", true);
+        targetPosition = GameObject.Find("TargetPosition").transform;
         isWalking = true;
+    }
+
+    public void ActivateMoose()
+    {
+        isActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Car") && mooseActivator != null)
+        {
+            if (other.gameObject.transform.position == mooseActivator.position)
+            {
+                StartWalking();
+            }
+        }
     }
 }
